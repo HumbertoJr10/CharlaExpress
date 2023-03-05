4f9247bc5ea76f6f86c0728c17bdb504b3af3f4e
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-03-05 12:56:44 -0400

    style(home): :lipstick: beta version of home

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,17 +1,25 @@
 import { hook_Home } from './hook_Home';
 import styles from './Home.module.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import girlSms from "../../assets/girl-sms.png";
+import { Link } from 'react-router-dom';
 
 function Home() {
 
   const { user, loginWithRedirect } = useAuth0()
-  
 
   return (
     <div className={styles.Home_Container}>
-      <h1>hola</h1>
-      <button onClick={()=> loginWithRedirect()}>Login</button>
-      
+      <div className={styles.textSide}>
+        <p>Bienvenidos a CharlaExpress, un espacio donde puedes conectarte con personas de todo el mundo y compartir tus pensamientos, ideas y experiencias. Ya sea que busques conocer gente nueva, discutir temas de interés o simplemente pasar un buen rato, nuestro chat es el lugar perfecto para ti. Únete a nuestra comunidad amigable y disfruta de la diversión y el entretenimiento que ofrecemos. ¡Esperamos verte pronto en nuestro chat!</p>
+        <Link to={"/chat"}>
+          <button>Chatear</button>
+        </Link>
+      </div>
+      <div className={styles.imgSide}>
+        <div className={styles.imgDecoration}></div>
+        <img src={girlSms} />
+      </div>
     </div>
   )
 }
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,20 @@
 import styles from "./Navbar.module.css";
 import chatIcon from "../../assets/chatIcon.svg"
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
-  const { user, isAuthenticated } = useAuth0()
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0()
 
   console.log(user)
 
   return (
     <nav className={styles.Navbar_Container}>
-      <div className={styles.Navbar_leftSide}>
+      <Link className={styles.Navbar_leftSide} to={"/"}> 
         <img src={chatIcon} />
         <h1>Charla<strong>Express</strong></h1>
-      </div>
+      </Link>
       <div className={styles.Navbar_rigthSide}>
         {
           isAuthenticated &&
@@ -22,6 +23,10 @@ function Navbar() {
         {
           isAuthenticated && 
             <img src={user?.picture} />
+        }
+        {
+          !isAuthenticated &&
+            <button onClick={()=> loginWithRedirect()}>Login</button>
         }
       </div>
     </nav>
