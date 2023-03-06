549a03120748f242bda31a48ba7bc4d8d32cbcd8
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-03-05 20:09:10 -0400

    style(Chat): :lipstick: Chat style and date improved

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -3,6 +3,7 @@ import styles from './Chat.module.css';
 import { hook_Chat } from './hook_Chat';
 import { useSelector } from 'react-redux';
 import { iGlobalMessage, iState } from '../../interface';
+import moment from 'moment';
 
 // ----------SVG --------------
 import sendBlue from '../../assets/blueSend.svg';
@@ -13,20 +14,24 @@ function Chat () {
   
   const { text, messages, chatContainerRef, DeleteMessage, HandlerChange, MessageSubbmit } = hook_Chat()
 
+  console.log(messages)
   
   return (
     <div className={styles.Chat_Overlay}>
       <main className={styles.Chat_Container} ref={chatContainerRef}>
           {
-            
             messages.map((chat, index) => (
               <div key={index + "Message"} className={styles.chat_MessageOverlay}>
+                <img src={chat.author.picture} />
                 <div className={styles.Chat_MessageContainer} key={index}>
                     <div className={styles.Chat_MessageAuthor}>
                       <p>{chat.author.username}</p>
                     </div>
                     <div className={styles.Chat_MessageContent}>
                       <p>{chat.message}</p>
+                    </div>
+                    <div className={styles.Chat_Date}>
+                      <p>{moment(chat.date).calendar()}</p>
                     </div>
                 </div>
               </div>
